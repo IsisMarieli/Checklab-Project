384f2bc19676b3a81bfd5ecdeda6af9a2a628fdc
IsisMarieli <Isismarieli1@gmail.com>
2025-07-11 00:57:36 -0300

    commit client

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,33 +1,93 @@
 import Header from '../../components/Header/Header';
 import styles from './Dashboard.module.css';
-import Sair from '../../assets/Sair.svg';
 import { useNavigate } from 'react-router-dom';
-import CardEntrada from '../../components/CardEntrada/CardEntrada';
-import CardSaida from '../../components/CardSaida/CardSaida';
-import CardHorasTotais from '../../components/CardHorasTotais/CardHorasTotais';
+import LogoutIcon from '../../assets/Saida.svg';
+import EntryIcon from '../../assets/Entrada.svg';
+import ExitIcon from '../../assets/Sair.svg';
+import TotalHoursIcon from '../../assets/HorasTotais.svg';
+import UserIcon from '../../assets/User.svg';
 
 function Dashboard() {
   const navigate = useNavigate();
 
-  function handleDashboard() {
-    navigate('/'); // redirecionar para Login
+  // Função para lidar com o logout, que redireciona para a página de login
+  function handleLogout() {
+    navigate('/'); // Redireciona para a rota de login
   }
 
   return (
     <div>
       <Header />
       <main className={styles.container}>
-        <div className={styles.WrapperTitle}>
-          <p className={styles.title}>Consultar presença - Laboratório Embarque Digital</p>
-          <button onClick={handleDashboard} className={styles.btnSair}>Sair <img src={Sair} alt="Botão Sair" /></button>
+        <div className={styles.wrapperTitle}>
+          <h1 className={styles.mainTitle}>Consultar presença - Laboratório Embarque Digital</h1>
+          <button onClick={handleLogout} className={styles.btnLogout}>
+            Sair <img src={LogoutIcon} alt="Ícone de Sair" />
+          </button>
         </div>
+        <div className={styles.cards}>
 
-        <div className={styles.cardWrapper}>
-          <CardEntrada />
-          <CardSaida />
-          <CardHorasTotais/>
+          {/* Card: Entradas Registradas */}
+          <div className={styles.card}>
+            <div className={styles.cardLeft}>
+              <h3 className={styles.cardTitle}>Entradas Registradas</h3>
+              <strong>6</strong>
+            </div>
+            <img src={EntryIcon} alt="Ícone de Entradas" />
+          </div>
+
+          {/* Card: Saídas Registradas */}
+          <div className={styles.card}>
+            <div className={styles.cardLeft}>
+              <h3 className={styles.cardTitle}>Saídas Registradas</h3>
+              <strong>6</strong>
+            </div>
+            <img src={ExitIcon} alt="Ícone de Saídas" />
+          </div>
+
+          {/* Card: Total de horas no lab */}
+          <div className={styles.card}>
+            <div className={styles.cardLeft}>
+              <h3 className={styles.cardTitle}>Total de horas no lab</h3>
+              <strong>8</strong>
+            </div>
+            <img src={TotalHoursIcon} alt="Ícone de Horas Totais" />
+          </div>
+
+          {/* Card: Informações do Embarcante */}
+          <div className={styles.card}>
+            <div className={styles.cardLeft}>
+              <h3 className={styles.cardTitle}>Informações do Aluno(a)</h3>
+              <p>Nome: <span>Isis Marieli</span></p>
+              <p>IES: <span>UNINASSAU</span></p>
+              <p>E-mail: <span>Isismarieli1@gmail.com</span></p>
+            </div>
+            <img src={UserIcon} alt="Ícone de Usuário" />
+          </div>
+
+          {/* Card: Horas totais por mês */}
+          <div className={styles.card}>
+            <div className={styles.cardLeft}>
+              <h3 className={styles.cardTitle}>Horas totais por mês</h3>
+              <strong>8</strong>
+            </div>
+            <img src={EntryIcon} alt="Ícone de Entradas" />
+          </div>
+
+          {/* Card: Conquistas no Lab */}
+          <div className={styles.card}>
+            <div className={styles.cardLeft}>
+              <h3 className={styles.cardTitle}>Minhas Conquistas no Lab</h3>
+            </div>
+          </div>
+
+          {/* Card: Presenças por mês */}
+          <div className={styles.card}>
+            <div className={styles.cardLeft}>
+              <h3 className={styles.cardTitle}>Presenças por mês</h3>
+            </div>
+          </div>
         </div>
-
       </main>
     </div>
   );

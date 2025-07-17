34a5bc0fcc8eee5bbfc598e4c33c3d4737a7c176
IsisMarieli <Isismarieli1@gmail.com>
2025-07-16 21:58:12 -0300

    commit frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from "./pages/Login/Login"
-import Dashboard from "./pages/Dashboard/Dashboard"
+import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
-
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Checklab-Project">
       <Routes>
-        <Route path="/Checklab-Project" element={<Login/>}/>
-        <Route path="/Checklab-Project" element={<Dashboard/>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
